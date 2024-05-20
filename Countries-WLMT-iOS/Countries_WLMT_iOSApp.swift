3de22a26914ff310777a1ec463df4c809561a57c
Israel <odunmeshileya1@gmail.com>
2024-05-19 20:18:09 -0400

    Initial Commit

---
+++ Countries-WLMT-iOS/Countries_WLMT_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Countries_WLMT_iOSApp.swift
+//  Countries-WLMT-iOS
+//
+//  Created by tech on 19/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Countries_WLMT_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Countries-WLMT-iOSUITests/Countries_WLMT_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Countries_WLMT_iOSUITestsLaunchTests.swift
+//  Countries-WLMT-iOSUITests
+//
+//  Created by tech on 19/05/2024.
+//
+
+import XCTest
+
+final class Countries_WLMT_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
